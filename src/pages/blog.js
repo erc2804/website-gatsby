16d748f7eb23a9d684cc56fce5466b054b13bb4e
erc2804 <erc2804@outlook.de>
2024-01-28 17:40:03 +0100

    blog ready

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -17,10 +17,19 @@ export default function Blog({ data }) {
       imageUrl: edge.node.frontmatter.imageUrl,
       verticalImgPosInPercent: edge.node.frontmatter.verticalImgPosInPercent,
       htmlContent: edge.node.html,
+      isOpen: false
     }));
 
     setBlogPosts(posts);
   }, [data]);
+
+  const toggleBlogEntry = (id) => {
+    setBlogPosts((prevBlogPosts) =>
+      prevBlogPosts.map((post) =>
+        post.id === id ? { ...post, isOpen: !post.isOpen } : post
+      )
+    );
+  };
 
   return (
     <Layout>
@@ -29,34 +38,36 @@ export default function Blog({ data }) {
         <div className="flex flex-col gap-10">
           {blogPosts.map((blogPost, idx) => (
             <article key={idx} className="w-full">
-              <button className="w-full h-52 rounded-md overflow-hidden relative group">
+              <button className="w-full h-52 rounded-md overflow-hidden relative group" onClick={() => toggleBlogEntry(blogPost.id)}>
                 <div
                   className="size-full bg-no-repeat bg-[length:100%_auto] transform duration-[10000ms] xl:group-hover:scale-125"
                   style={{ backgroundImage: "url(" + blogPost.imageUrl + ")", backgroundPositionY: blogPost.verticalImgPosInPercent + "%" }}
                 ></div>
                 <div className="absolute inset-0 bg-transparency-medium-lvl duration-500 xl:group-hover:bg-transparency-low-lvl z-10" />
                 <div className="p-2 flex flex-row gap-6 items-end absolute inset-0 z-20">
-                  <div className="flex-1 flex flex-col gap-3">
+                  <div className={`flex-1 flex flex-col gap-3 ${blogPost.isOpen ? 'translate-y-full' : ''} transition-transform`}>
                     <div className="px-0.5 ec-font-heading-1 text-brand-green-medium-lvl text-left">{blogPost.title}</div>
                     <div className="flex flex-row gap-2 items-center">
                       <CalendarIcon iconClasses="size-6 fill-brand-sand" />
                       <span className="text-brand-sand">{blogPost.date}</span>
                     </div>
                   </div>
-                  <ChevronDownIcon iconClasses="flex-none size-6 fill-brand-sand transition-transform xl:group-hover:rotate-12" />
+                  <ChevronDownIcon iconClasses={`flex-none size-6 fill-brand-sand ${blogPost.isOpen ? 'rotate-180 xl:group-hover:rotate-[168deg]' : 'xl:group-hover:rotate-12'} transition-transform `} />
                 </div>
               </button>
-              <div className="flex flex-col ec-layout-text-content">
-                <div className="flex flex-row mt-7 text-left">
-                  <span className="ec-font-heading-1 first:pr-4">{blogPost.title}</span>
-                  <span>{blogPost.date}</span>
+              {blogPost.isOpen && (
+                <div className="flex flex-col ec-layout-text-content">
+                  <div className="flex flex-row gap-3 justify-between mt-7 text-left">
+                    <span className="flex-1 ec-font-heading-1 first:pr-4">{blogPost.title}</span>
+                    <span className="flex-none mt-1.5">{blogPost.date}</span>
+                  </div>
+                  <div
+                    className="prose"
+                    dangerouslySetInnerHTML={{ __html: blogPost.htmlContent }}
+                  ></div>
+                  <hr className="border-gray-low-lvl" />
                 </div>
-                <div
-                  className="prose"
-                  dangerouslySetInnerHTML={{ __html: blogPost.htmlContent }}
-                ></div>
-                <hr className="border-gray-low-lvl" />
-              </div>
+              )}
             </article>
           ))}
         </div>
