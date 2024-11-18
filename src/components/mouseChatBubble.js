cd1adf377010b7729d81131254c566bede3edc2b
erc2804 <erc2804@outlook.de>
2024-11-18 20:17:17 +0100

    chatbubble fixes

--- src/components/mouseChatBubble.js
+++ src/components/mouseChatBubble.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState, useCallback, useRef } from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import { injectIntl } from "gatsby-plugin-intl"
+import TypingAnimation from "./chat/typingAnimation"
 
 const MouseChatBubble = ({ intl }) => {
   const [chatBubbleVisible, setChatBubbleVisible] = useState(false)
   const [currentChatMessage, setCurrentChatMessage] = useState(
-    intl.formatMessage({ id: 'chat-bubble.starter' })
+    intl.formatMessage({ id: "chat-bubble.starter" })
   )
-  const [fadeOut, setFadeOut] = useState(false)
+  const [newMessageLoading, setNewMessageLoading] = useState(false)
 
-  const [elementPosStyles, setElementPosStyles] = useState({ top: 0, right: 0 })
+  const [elementPosStyles, setElementPosStyles] = useState({ top: -1000, right: -1000 })
   const wrapperRef = useRef(null)
   const forceStarterMessageVisibility = useRef(true)
   const [dataChatElements, setDataChatElements] = useState([])
@@ -29,44 +30,53 @@ const MouseChatBubble = ({ intl }) => {
     }, 2000)
   }, [])
 
-  const updatePositionAndMessage = useCallback((event) => {
-    const { clientX, clientY, target } = event
-    const viewportHeight = window.innerHeight
-    const viewportWidth = window.innerWidth
-    const avatarSize = 40
-    const safeSpace = 12
+  const updatePositionAndMessage = useCallback(
+    (event) => {
+      const { clientX, clientY, target } = event
+      const viewportHeight = window.innerHeight
+      const viewportWidth = window.innerWidth
+      const avatarSize = 40
+      const safeSpace = 12
 
-    const maxTop = viewportHeight - (wrapperRef.current?.offsetHeight + 64 ?? 0)
-    const top = Math.min(Math.max(80, clientY - avatarSize / 2), maxTop)
+      const maxTop =
+        viewportHeight - (wrapperRef.current?.offsetHeight + 64 ?? 0)
+      const top = Math.min(Math.max(80, clientY - avatarSize / 2), maxTop)
 
-    const maxRight = viewportWidth - (wrapperRef.current?.offsetWidth + 3 ?? 0)
-    const right = Math.min(Math.max(0, viewportWidth - clientX - avatarSize - safeSpace * 2), maxRight)
+      const maxRight =
+        viewportWidth - (wrapperRef.current?.offsetWidth + 3 ?? 0)
+      const right = Math.min(
+        Math.max(0, viewportWidth - clientX - avatarSize - safeSpace * 2),
+        maxRight
+      )
 
-    setElementPosStyles({ top, right })
-    if (!chatBubbleVisible) startVisibilityTimer()
+      setElementPosStyles({ top, right })
+      if (!chatBubbleVisible) startVisibilityTimer()
 
-    // --- directly update position but debounce message update
-    handleMessageChange(target, clientX, clientY)
-  }, [chatBubbleVisible, startVisibilityTimer, dataChatElements, currentChatMessage])
-
-  const debounceMessageChange = useRef(null)
+      handleMessageChange(target, clientX, clientY)
+    },
+    [
+      chatBubbleVisible,
+      startVisibilityTimer,
+      dataChatElements,
+      currentChatMessage,
+    ]
+  )
 
   const handleMessageChange = (target, clientX, clientY) => {
-    if (debounceMessageChange.current) {
-      clearTimeout(debounceMessageChange.current)
-    }
+    let newChatMessage = null
+    dataChatElements.forEach((el) => {
+      if (el.contains(target)) newChatMessage = el.getAttribute("data-chat")
+    })
 
-    debounceMessageChange.current = setTimeout(() => {
-      let newChatMessage = null
-      dataChatElements.forEach((el) => {
-        if (el.contains(target)) newChatMessage = el.getAttribute("data-chat")
+    if (
+      !forceStarterMessageVisibility.current &&
+      newChatMessage &&
+      currentChatMessage !== newChatMessage
+    ) {
+      triggerMessageChangeAnim(() => {
+        setCurrentChatMessage(newChatMessage)
       })
-
-      if (!forceStarterMessageVisibility.current && newChatMessage && currentChatMessage !== newChatMessage) {
-        triggerFadeOut(() => {
-          setCurrentChatMessage(newChatMessage)
-        })
-      } 
+    }
     //   else {
     //     const distanceToElem = calcDistanceToElemContainingCurrentMessage(clientX, clientY)
     //     if (distanceToElem >= 250) {
@@ -75,19 +85,15 @@ const MouseChatBubble = ({ intl }) => {
     //       })
     //     }
     //   }
-    }, 200)
   }
 
-  // --- fadeout of message on change
-  const triggerFadeOut = (callback) => {
-    setFadeOut(false) // --- clear ongoing fade-out
+  // --- anim on message on change
+  const triggerMessageChangeAnim = (callback) => {
+    setNewMessageLoading(true)
     setTimeout(() => {
-      setFadeOut(true) 
-      setTimeout(() => {
-        callback()
-        setFadeOut(false)
-      }, 300)
-    }, 10) // --- delay to ensure fade-out reset
+      callback()
+      setNewMessageLoading(false)
+    }, 1000)
   }
 
   // --- calc distance to the element containing the current message
@@ -99,7 +105,9 @@ const MouseChatBubble = ({ intl }) => {
       const rect = currentElem.getBoundingClientRect()
       const elemCenterX = rect.left + rect.width / 2
       const elemCenterY = rect.top + rect.height / 2
-      return Math.sqrt(Math.pow(clientX - elemCenterX, 2) + Math.pow(clientY - elemCenterY, 2))
+      return Math.sqrt(
+        Math.pow(clientX - elemCenterX, 2) + Math.pow(clientY - elemCenterY, 2)
+      )
     }
     return 0
   }
@@ -120,7 +128,9 @@ const MouseChatBubble = ({ intl }) => {
       ref={wrapperRef}
     >
       <div
-        className={`flex flex-none transition-transform duration-500 transform ${chatBubbleVisible ? "rotate-0 scale-100" : "-rotate-90 scale-0"}`}
+        className={`flex flex-none transition-transform duration-500 transform ${
+          chatBubbleVisible ? "rotate-0 scale-100" : "-rotate-90 scale-0"
+        }`}
       >
         <StaticImage
           src="../images/pixelcan.png"
@@ -129,11 +139,16 @@ const MouseChatBubble = ({ intl }) => {
         />
       </div>
       <div
-        className={`flex-none px-2 pt-px max-w-[200px] bg-brand-sand rounded-xl duration-500 delay-300 transition-opacity ${chatBubbleVisible ? "opacity-100" : "opacity-0"}`}
+        className={`flex-none flex flex-row items-center min-h-[25px] px-2 pt-px max-w-[200px] bg-brand-sand rounded-xl duration-500 delay-300 transition-opacity ${
+          chatBubbleVisible ? "opacity-100" : "opacity-0"
+        }`}
       >
-        <span className={`duration-150 transition-opacity ${fadeOut ? "opacity-0" : "opacity-100"}`}>
-          {currentChatMessage}
-        </span>
+        {newMessageLoading && <TypingAnimation />}
+        {!newMessageLoading && (
+          <span className="duration-150 transition-opacity">
+            {currentChatMessage}
+          </span>
+        )}
       </div>
     </div>
   )
